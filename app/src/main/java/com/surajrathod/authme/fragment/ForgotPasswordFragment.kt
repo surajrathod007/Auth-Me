7bfa8774cbb1cae01805b53a60b7e5f774c4b268
Manshal Khatri <khatrimanshal1206@gmail.com>
2022-07-03 15:45:49 +0530

    Completed ForgotPasswordFragment.kt

--- app/src/main/java/com/surajrathod/authme/fragment/ForgotPasswordFragment.kt
+++ app/src/main/java/com/surajrathod/authme/fragment/ForgotPasswordFragment.kt
@@ -1,5 +1,6 @@
 package com.surajrathod.authme.fragment
 
+import android.app.Dialog
 import com.surajrathod.authme.R
 
 import android.content.Context
@@ -11,25 +12,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.surajrathod.authme.databinding.FragmentForgotPasswordBinding
+import com.surajrathod.authme.model.LoginReq
+import com.surajrathod.authme.model.SimpleResponse
+import com.surajrathod.authme.network.NetworkService
+import com.surajrathod.authme.util.ExceptionHandler
 import com.surajrathod.authme.util.LoadingScreen
+import kotlinx.coroutines.launch
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ForgotPasswordFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ForgotPasswordFragment : Fragment() {
-
-
     private lateinit var binding : FragmentForgotPasswordBinding
-
+    lateinit var d : LoadingScreen
+    lateinit var dd : Dialog
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -37,13 +33,20 @@ class ForgotPasswordFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_forgot_password, container, false)
         binding = FragmentForgotPasswordBinding.bind(view)
-
+         d = LoadingScreen(activity as Context)
+         dd = d.loadingScreen()
         binding.btnReset.setOnClickListener {
             if(!TextUtils.isEmpty(binding.ETEmail.text.toString().trim(){it <= ' ' })){
-                val d = LoadingScreen(activity as Context)
-                val dd = d.loadingScreen()
                 d.toggleDialog(dd)
-
+                    lifecycleScope.launch{
+                        try {
+                            val response = NetworkService.networkInstance.sendOtp(binding.ETEmail.text.toString())
+                            onSimpleResponse("Sent",response)
+                        }catch (e : Exception){
+                            activity?.let { ExceptionHandler.catchOnContext(it, e) }
+                            d.toggleDialog(dd)
+                        }
+                    }
             }else{
                 Toast.makeText(activity, "please enter email address", Toast.LENGTH_SHORT).show()
             }
@@ -51,5 +54,14 @@ class ForgotPasswordFragment : Fragment() {
 
         return view
     }
-
+    fun onSimpleResponse(task : String,simpleResponse: SimpleResponse){
+        if(simpleResponse.success){
+            d.toggleDialog(dd)  // hide
+            Toast.makeText(activity, "$task Successful", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_forgotPasswordFragment_to_resetPasswordFragment)
+        }else{
+            d.toggleDialog(dd)  // hide
+            Toast.makeText(activity, simpleResponse.message, Toast.LENGTH_SHORT).show()
+        }
+    }
 }
