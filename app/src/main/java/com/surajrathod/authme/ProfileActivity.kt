44d3994c82026ab178d852fe6b9696db5a282df4
Manshal Khatri <khatrimanshal1206@gmail.com>
2022-07-04 20:02:47 +0530

    Added feature to store JWT Token on device

--- app/src/main/java/com/surajrathod/authme/ProfileActivity.kt
+++ app/src/main/java/com/surajrathod/authme/ProfileActivity.kt
@@ -2,10 +2,16 @@ package com.surajrathod.authme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.surajrathod.authme.util.DataStore
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val token = intent.getStringExtra(DataStore.JWT_TOKEN)
+        Toast.makeText(this, token, Toast.LENGTH_SHORT).show() // remove it after testing
+
     }
 }
--- app/src/main/java/com/surajrathod/authme/fragment/LoginFrag.kt
+++ app/src/main/java/com/surajrathod/authme/fragment/LoginFrag.kt
@@ -9,6 +9,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.datastore.dataStore
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -18,6 +22,8 @@ import com.surajrathod.authme.databinding.FragLoginBinding
 import com.surajrathod.authme.model.LoginReq
 import com.surajrathod.authme.model.SimpleResponse
 import com.surajrathod.authme.network.NetworkService
+import com.surajrathod.authme.util.DataStore
+import com.surajrathod.authme.util.DataStore.preferenceDataStoreAuth
 import com.surajrathod.authme.util.ExceptionHandler
 import com.surajrathod.authme.util.LoadingScreen
 import kotlinx.coroutines.launch
@@ -74,11 +80,21 @@ class LoginFrag : Fragment() {
     fun onSimpleResponse(task : String,simpleResponse: SimpleResponse){
         if(simpleResponse.success){
             d.toggleDialog(dd)  // hide
+            lifecycleScope.launch{
+                storeStringPreferences(DataStore.JWT_TOKEN,simpleResponse.message)
+            }
             Toast.makeText(activity, "$task Successful", Toast.LENGTH_SHORT).show()
             // TODO : Navigation to ProfileActivity / DashboardActivity
         }else{
             d.toggleDialog(dd)  // hide
             Toast.makeText(activity, simpleResponse.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+    suspend fun storeStringPreferences(key: String ,value : String){
+        activity?.let{ dsContext ->
+            dsContext.preferenceDataStoreAuth.edit {
+                it[stringPreferencesKey(key)] = value
+            }
         }
     }
 }
